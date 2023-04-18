3c94d0af6e7a093a54cb01d6439a0f7031ab987d
maxb6299 <126419161+maxb6299@users.noreply.github.com>
2023-04-18 19:31:42 -0400

    Write most of menu class

---
+++ menu.cpp
@@ -0,0 +1,155 @@
+// menu.cpp
+
+#include "system.cpp"
+#include <iostream>
+using namespace std;
+
+
+class Menu
+{
+public:
+    Menu()
+    {
+        system = System();
+        mainMenu();
+    }
+    char getInput() // accepts input and turns it uppercase (for easier boolean)
+    {
+        char c;
+        cout << ">";
+        cin >> c;
+        toupper(c);
+        return c;
+    }
+    void mainMenu() // loops options until user inputs q to quit
+    {
+        char input;
+
+        while (input != 'Q')
+        {
+            cout << "    Main Menu\n\n"
+                 << "C - Create Account\n"
+                 << "S - Sign In\n"
+                 << "Q - Quit Program\n";
+
+            input = getInput();
+            
+            if (input == 'C') {menuCreateAccount();}
+            else if (input == 'S') {menuSignIn();}
+            else if (input != 'Q') {cout << "Invalid Input. Please try again.\n";}
+        }
+    }
+    void menuCreateAccount()
+    {
+        string u;
+        string p;
+        int b;
+
+        cout << "Username: ";
+        cin >> u;
+        cout << "Password";
+        cin >> p;
+        cout << "Initial Deposit: ";
+        cin >> b;
+
+        if (b < 0) 
+        {cout << "Error: Negative deposit. Setting deposit to $0.\n";}
+        
+        if (!system.createAccount(u, p, b))
+        {
+            cout << "Error: Username Already Taken. Please try again.\n";
+            menuCreateAccount();
+        }
+    }
+    void menuSignIn()
+    {
+        string u;
+        string p;
+
+        cout << "Username: ";
+        cin >> u;
+        cout << "Password: ";
+        cin >> p;
+
+        if (!system.signIn(u, p))
+        {
+            cout << "Error. Invalid Credentials.\n";
+            return;
+        }
+        accountMenu();
+    }
+    void accountMenu()
+    {
+        char input;
+
+        while (input != 'S')
+        {
+            viewAccountMenu();
+            input = getInput();
+            
+            if (input == 'C') {menuCreateAccount();}
+            else if (input == 'D') {menuDeposit();}
+            else if (input == 'W') {menuWithdrawal();}
+            else if (input == 'V') {menuViewTransactions();}
+            else if (input == 'M') {viewAccountMenu();}
+            else if (input == 'R') {menuDeleteAccount();}
+            else if (input == 'S') {menuSignOut();}
+            else {cout << "Invalid Input. Please try again.\n";}
+        }
+    }
+    void menuDeposit()
+    {
+        int n;
+
+        cout << "How much would you like to deposit? ";
+        cin >> n;
+        
+        system.currentUser->deposit(n);
+    }
+    void menuWithdrawal()
+    {
+        int n;
+
+        cout << "How much would you like to withdrawal? ";
+        cin >> n;
+        
+        system.currentUser->withdrawal(n);
+    }
+    void menuViewTransactions()
+    {
+        vector<int> t = system.currentUser->getTransactions();
+
+
+        cout << "Your transaction history is: \n";
+        for (int i = 0; i < t.size(); i++)
+        {
+            cout << t[i] << "\n";
+        }
+    }
+    void viewAccountMenu()
+    {
+        cout << "    Welcome\n\n"
+             << "D - Deposit\n"
+             << "W - Withdrawal\n"
+             << "V - View Transaction History\n"
+             << "M - View This Menu\n"
+             << "R - Delete Account\n"
+             << "S - Sign Out\n";
+    }
+    void menuDeleteAccount()
+    {
+        // TODO
+    }
+    void menuSignOut()
+    {
+        system.signOut();
+    }
+
+private:
+    System system;
+};
+
+int main()
+{
+    Menu menu;
+}
